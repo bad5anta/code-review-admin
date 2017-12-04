15a81053acb6a66411d74ac923d6e71e9a9f1021
Vadim Checherynda <checherinda.vadym@gmail.com>
2017-12-04 14:44:36 +0200

    Add bootstrap and multiselect

--- src/content/form/main/index.js
+++ src/content/form/main/index.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import { FormGroup,ControlLabel,FormControl } from 'react-bootstrap';
+import MultiplySelelct from './multiplySelelct';
 import './style.css';
 
 class Main extends Component {
   render() {
     return (
       <form>
-        <header>Form</header>
-
         <div className="User">
-          <input type="text" />
-          <input type="text" />
+          <FormGroup
+            controlId="formBasicText">
+            <ControlLabel>User:</ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Enter text"/>
+            <FormControl.Feedback />
+            <MultiplySelelct />
+          </FormGroup>
+
         </div>
       </form>
     );
